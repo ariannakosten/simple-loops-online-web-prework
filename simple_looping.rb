e30c5c788b56a1423798a760058387c487fa0e46
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-15 03:34:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 def for_iterator(number_of_times)
   counter = 1..7
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for int in number_of_times puts phrase 
+  for int in for_iterator puts phrase 
   end
 end
 end 
